2637675360af2593fb68d75f5ba60e96c6e126d3
Michael Propp <michaelpropp8664@gmail.com>
2020-05-10 12:31:25 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'To-Do List',
+      home: TaskList(),
+    );
+  }
+}
+
+class TaskList extends StatefulWidget {
+  @override
+  _TaskListState createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text("Hello World"));
+  }
+}
